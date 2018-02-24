453d8dc28fb0c3631b0835685ada4749670b3cd3
Tony <yxin89@sina.com>
2018-02-24 21:16:06 +0800

    Delete MongoDB.bat

--- MongoDB.bat
+++
@@ -1,5 +0,0 @@
-net stop MongodbTest
-sc delete MongodbTest
-E:\Applications\MongoDB\bin\mongod --install --serviceName MongodbTest --serviceDisplayName MongodbTest --config E:\Applications\MongoDB\mongod.cfg
-net start MongodbTest
-pause
